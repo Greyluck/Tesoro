11eb9624114707d1dfcc6b1ce8d2ad52602741c5
Greyluck <36477858+Greyluck@users.noreply.github.com>
2023-09-15 03:37:36 -0300

    Update de manual

--- main.cpp
+++ main.cpp
@@ -28,58 +28,123 @@ int** createBoard(int columns, int rows){
 void printBoard(int** board,int columns,int rows){
     printf("TABLERO DE JUEGO: \n");
 
-    // Crea un array con 20 letras usadas para identificar las casillas.
-    std::string letters[20] = {"A","B","C","D","E","F","G","H","I","J",
-                       "K","L","M","N","O","P","Q","R","S","T"};
-
-
     // Lo que se agregue en esta seccion se imprimie una sola vez
-    std::cout << " |";
+    std::cout << "  |";
     for (int column = 0; column < columns; column++){
-        std::string myLetter = letters[column];
-        std::cout << myLetter;
+        if (column < 10){
+            std::cout << "0" << column << "|";
+        }
+        else{
+            std::cout << column  << "|";
+        }
     }
     std::cout << "\n";
 
     // TODO: Por algun motivo la impresion del tablero se debe hacer opuesta a su creacion. Revisar luego.
     for (int row = 0; row < rows; row++){
         // Lo que se agregue en esta seccion se imprimie en cada linea
-        std::string myLetter = letters[row];
-        std::cout << myLetter <<"|";
+        if (row < 10){
+            std::cout << "0" << row << "|";
+        }
+        else{
+            std::cout << row  << "|";
+        }
 
         for (int column = 0; column < columns; column++){
             // Lo que se agregue en esta seccion se imprime en cada celda
-            printf("%i", board[column][row]);
+            printf(" %i ", board[column][row]);
         }
-        printf("\n");
+        printf("|\n");
     }
-    printf("\n");
 }
 
-/** Solicita al jugador ingresar una celda para colocar su tesoro*/
-void placeChest(){
-    char column, row;
-    std::cout << "Elige la columna donde colocaras el tesoro: ";
+/** Solicita al jugador ingresar una celda para colocar un unico tesoro*/
+int** placeChest(int** board,int player){
+    const int EMPTY = 0;
+    const int CHEST1 = 1;
+    const int CHEST2 = -1;
+    const int BOTHCHESTS = 3;
+
+    int column, row;
+    std::cout << "Jugador "<< player <<", elige la columna donde colocaras el nuevo tesoro: \n";
     std::cin >> column;
-    std::cout << "Elige la fila donde colocaras el tesoro: ";
+    std::cout << "Elige la fila donde colocaras este tesoro: \n";
     std::cin >> row;
-    std::cout << "Eligiste la columna "<< column <<" y la fila "<< row;
+    std::cout << "Eligiste la columna "<< column <<" y la fila "<< row << " para esconder este tesoro.\n";
+    if (board[column][row] == EMPTY){
+        board[column][row] = player;
+    }
+    else{
+        if (board[column][row] == CHEST1 || board[column][row] == CHEST2){
+            board[column][row] = BOTHCHESTS;
+        }
+    }
+
+    return board;
 }
 
-/** Solicita al jugador ingresar una celda para colocar su epia*/
+/** Solicita al jugador ingresar todos los tesoros iniciales*/
+int** placeChests(int** board){
+    const int CHESTQUANTITY = 4;
 
+    // El jugador 1 coloca sus tesoros primero
+    int player = 1;
+    for (int chest = 1; chest <= CHESTQUANTITY; chest++) {
+        board = placeChest(board,player);
+    }
+
+    // El jugador 2 coloca sus tesoros depues
+    player = 2;
+    for (int chest = 1; chest <= CHESTQUANTITY; chest++) {
+        board = placeChest(board,player);
+    }
+    return board;
+}
+
+/** Solicita al jugador ingresar una celda para colocar su espia*/
+void placeTheSpy(){
+}
+
+/** Pregunta al jugador si desea mover un tesoro*/
+void moveTheChesty(){
+}
+
+/** Exporta el tablero para que solo el jugador correspondiente vea los valores*/
+void exportBoard(){
+}
+
+/** Determina la sucesion de acciones que se realizan cada turno*/
+void playTheTurn(){
+
+}
 
 int main() {
+    // Determina que valor esta jugando.
+    int player = 1;
+
     // El tamaño del tablero es constante. Por lo tanto tambien sus filas y columnas
-    const int columns =20, rows = 20;
-    
+    const int COLUMNS = 20, ROWS = 20;
+
     // Crea el tablero que vamos a usar
-    int** myBoard = createBoard (columns,rows);
+    int **myBoard = createBoard(COLUMNS, ROWS);
 
-    // Muesta el tablero que vamos a usar
-    printBoard(myBoard,columns,rows);
+    // Muestra por consola el estado del tablero (Esto no sera visible para los jugadores)
+    printBoard(myBoard, COLUMNS, ROWS);
 
-    // Solicita al usuario que ingrese su tesoro.
-    placeChest();
+    // Solicita al usuario que ingrese sus tesoros.
+    myBoard = placeChests(myBoard);
+
+    // Muestra por consola el estado del tablero (Esto no sera visible para los jugadores)
+    printBoard(myBoard, COLUMNS, ROWS);
+
+    // Turno
+    playTheTurn();
+    /** TURNO:
+     *  - el jugador coloca un espia.
+     *  - el jugador recibe una notificacion de tesoro.
+     *  - el jugador mueve un tesoro.
+     *  - se exporta su tablero para que lo vea al terminar.
+     */
+
 }
 
