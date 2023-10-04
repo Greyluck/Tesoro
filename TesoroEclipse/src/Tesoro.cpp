3e40baacf7595dc99f7c68ce8bb001345d8e93ad
Greyluck <Emilio.o_91@hotmail.com>
2023-10-04 01:39:41 -0300

    Cambios menores

--- TesoroEclipse/src/Tesoro.cpp
+++ TesoroEclipse/src/Tesoro.cpp
@@ -7,11 +7,17 @@ using namespace std;
 int main() {
 	cout << "Starting program" << endl;
 	Board theBoard;
-    theBoard.setInitialValues(20,20,5);//TODO: Setear la cantidad de cofres
+    theBoard.setInitialValues(5,5,1);//TODO: Setear la cantidad de cofres
     theBoard.createMatrix();
     // Solicita a ambos usuarios que ingresen sus tesoros.
     theBoard.placeInitialChests();
 
+    // Turnos
+    while (!theBoard.returnVictoryStatus()){
+        theBoard.playTheTurn();
+    }
+    // borrar el tablero para liberar memoria;
+    theBoard.deleteMatrix(theBoard);
 
     return 0;
 }
